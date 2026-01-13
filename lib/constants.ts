271bd7038f5c07aaf0d02046bcd34b6baac4bfba
abmbodj <vriistt@gmail.com>
2026-01-13 16:30:22 -0500

    Add TradingView widget component and configuration constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,3 +4,29 @@ export const NAV_ITEMS = [
     { href: '/watchlist', label: 'Watchlist' },
 
 ]
+
+export const MARK_OVERVIEW_WIDGET_CONFIG = {
+
+          "allow_symbol_change": true,
+          "calendar": false,
+          "details": false,
+          "hide_side_toolbar": true,
+          "hide_top_toolbar": false,
+          "hide_legend": false,
+          "hide_volume": false,
+          "hotlist": false,
+          "interval": "D",
+          "locale": "en",
+          "save_image": true,
+          "style": "1",
+          "symbol": "NASDAQ:AAPL",
+          "theme": "dark",
+          "timezone": "Etc/UTC",
+          "backgroundColor": "#0F0F0F",
+          "gridColor": "rgba(242, 242, 242, 0.06)",
+          "watchlist": [],
+          "withdateranges": false,
+          "compareSymbols": [],
+          "studies": [],
+          "autosize": true
+        }
