a199b77424bed063be86d0878b5712b85c098053
abmbodj <vriistt@gmail.com>
2026-01-13 17:40:32 -0500

    Remove unused import and clean up TradingViewWidget component

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -19,7 +19,6 @@ const TradingViewWidget = ({ title, scriptURL, config, height = 600, className }
     <div className="w-full">
       {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
       <div className={cn('tradingview-widget-container', className)} ref={containerRef}>
-        <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }} />
       </div>
     </div>
   );
