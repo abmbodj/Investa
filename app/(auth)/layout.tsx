58d6be3dad56b30a727ad0e4dea8cf7f38537065
abmbodj <vriistt@gmail.com>
2026-01-15 10:46:52 -0500

    Add SignIn and SignUp components with basic structure

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,9 @@
-import Header from "@/components/Header";
+
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="min-h-screen text-gray-400">
-        
+
     </main>
   )
 }
