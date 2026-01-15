2c62660f270ad469b1f297bfd280a89d2b513249
abmbodj <vriistt@gmail.com>
2026-01-15 10:42:20 -0500

    Add layout component for authentication pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="min-h-screen text-gray-400">
+        
+    </main>
+  )
+}
+
+export default layout
